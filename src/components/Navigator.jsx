f4025b64f4ff0bc0e3b6f8a81354b3fe441e54eb
Andy McBeath <mcbeath1@gmail.com>
2023-02-21 09:54:07 -0600

    fixed social-media-links routing

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -4,6 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from "react-router-dom";
+import SocialMediaLinks from './social-media-links';
 
 
 export default function Navigator() {
