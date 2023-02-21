c2d67bc302c9f74251b59aeae2f8efd45d10b253
Andy McBeath <mcbeath1@gmail.com>
2023-02-21 10:15:19 -0600

    work you embeds

--- src/pages/social-media.jsx
+++ src/pages/social-media.jsx
@@ -1,6 +1,5 @@
-import { TwitterEmbed } from 'react-embed';
+import { Embed } from 'react-embed';
 import Container from "react-bootstrap/Container";
-import { InstagramEmbed } from 'react-embed';
 import Row from "react-bootstrap/Row";
 import Card from 'react-bootstrap/Card';
 
@@ -11,19 +10,19 @@ export default function SocialMedia() {
       <Row className="d-flex justify-content-center">
         <Card style={{ width: '50%', height: '75%', }} className="shadow mx-auto">
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-            <TwitterEmbed url="https://twitter.com/caught_steelin/status/1625233270483853329" width={600}/>
+            <Embed url="https://twitter.com/caught_steelin/status/1625233270483853329" width={600}/>
           </div>
         </Card>
         <Card style={{ width: '50%', height: '75%',}} className="shadow mx-auto">
           <div style={{ display: 'flex', justifyContent: 'center',  alignItems: 'center', height: '100%' }}>
-            <TwitterEmbed url="https://twitter.com/caught_steelin/status/1585717238476046336" width={600} />
+            <Embed url="https://twitter.com/caught_steelin/status/1585717238476046336" width={600} />
           </div>
         </Card>
       </Row>
       <Row className="d-flex justify-content-center align-items-center">
         <Card style={{ width: '50%', height: '75%' }} className="shadow mx-auto">
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-            <InstagramEmbed url="https://www.instagram.com/p/CnFtOjWPIk-/" width={600} />
+            <Embed url="https://www.instagram.com/p/CnFtOjWPIk-/" width={600} />
           </div>
         </Card>
       </Row>
