702b950596f1e014653a6baeda113b3a979ea42c
Andy McBeath <mcbeath1@gmail.com>
2023-02-21 10:31:32 -0600

    messin with twitter embeds

--- src/pages/social-media.jsx
+++ src/pages/social-media.jsx
@@ -1,8 +1,9 @@
-import { TwitterEmbed } from 'react-social-media-embed';
-import Container from "react-bootstrap/Container";
 import { InstagramEmbed } from 'react-social-media-embed';
+import Container from "react-bootstrap/Container";
+import { TwitterEmbed } from 'react-social-media-embed';
 import Row from "react-bootstrap/Row";
 import Card from 'react-bootstrap/Card';
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
 
 export default function SocialMedia() {
   return (
@@ -11,7 +12,11 @@ export default function SocialMedia() {
       <Row className="d-flex justify-content-center">
         <Card style={{ width: '50%', height: '75%', }} className="shadow mx-auto">
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-            <TwitterEmbed url="https://twitter.com/caught_steelin/status/1625233270483853329" width={600}/>
+            <TwitterTimelineEmbed
+              sourceType="profile"
+              screenName="saurabhnemade"
+              options={{height: 400}}
+            />
           </div>
         </Card>
         <Card style={{ width: '50%', height: '75%',}} className="shadow mx-auto">
@@ -24,6 +29,15 @@ export default function SocialMedia() {
         <Card style={{ width: '50%', height: '75%' }} className="shadow mx-auto">
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
             <InstagramEmbed url="https://www.instagram.com/p/CnFtOjWPIk-/" width={600} />
+          </div>
+        </Card>
+      </Row>
+      <Row className="d-flex justify-content-center align-items-center">
+        <Card style={{ width: '50%', height: '75%' }} className="shadow mx-auto">
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+            <TwitterFollowButton
+            screenName={'caught_steelin'}
+            />
           </div>
         </Card>
       </Row>
