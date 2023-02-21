4f89a1fb72d62980cf819b0282f53141ff958b7f
Andy McBeath <mcbeath1@gmail.com>
2023-02-21 10:57:01 -0600

    got rid of social media

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,8 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Booking from "./pages/Booking";
-import  Merch from "./pages/Merch";
 import AboutUs from "./pages/AboutUs";
 import  YoutubeVids  from "./pages/YoutubeVids";
-import SocialMedia from './pages/social-media'
 import MailingList from "./pages/MailingList";
 import CalendarEvents from "./pages/Calendar";
 
@@ -22,11 +20,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/Home" element={<Home /> } />
           <Route path="/Booking" element={<Booking />} />
-          <Route path="/Merch" element={<Merch />} />
           <Route path="/Calendar" element={<CalendarEvents />} />
           <Route path="/AboutUs" element={<AboutUs />} />
           <Route path="/YoutubeVids" element={<YoutubeVids />} />
-         <Route path="/social-media" element={<SocialMedia />} />
          <Route path="/mailing-list" element={<MailingList />} />
         </Routes>
       </BrowserRouter>
