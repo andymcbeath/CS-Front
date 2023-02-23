8e3487933499fac5609a1d517af96cc757f06b13
Andy McBeath <mcbeath1@gmail.com>
2023-02-23 12:42:12 -0600

    got rid of top logo

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -8,9 +8,6 @@ import Card from "react-bootstrap/Card"
 export default function AboutUs() {
   return (
     <div className="container">
-      <Row className="Logo-title" style={{ marginTop: "1rem"}}>
-        <img src="/assets/logo.png" alt="Caught-Steelin-Logo"  />
-      </Row>
       <Card className="shadow d-flex flex-column align-items-center" style={{ marginTop: "1rem"}}>
         <Bio />
       </Card>
