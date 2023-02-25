28b384226b1c8317bd64948e4f9cd4cb286607c5
Andy McBeath <mcbeath1@gmail.com>
2023-02-24 21:04:18 -0600

    changed how to access back end

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './App.css'
+import axios from "axios";
 
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://caught-back.fly.dev";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
